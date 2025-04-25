fb9eb2663ae723d33753d59ed666103200a9f541
pushkar1713 <pushkar1713@gmail.com>
2025-04-25 22:58:03 +0530

    db error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "drizzle-kit";
+const path = require("path");
+const fs = require("fs");
 
 export default {
   schema: "./db/migrations/0000_initial_schema.sql",
@@ -6,5 +8,9 @@ export default {
   dialect: "mysql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl: {
+      ca: fs.readFileSync(path.resolve(__dirname, "../certs/ca.crt")), // Adjust the path based on your file structure
+      rejectUnauthorized: true,
+    },
   },
 } satisfies Config;
