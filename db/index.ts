eef183c35247578ff9099130efe51b72a65d9d18
pushkar1713 <pushkar1713@gmail.com>
2025-04-25 23:23:35 +0530

    db error 6

--- db/index.ts
+++ db/index.ts
@@ -4,14 +4,6 @@ import * as schema from "./schema";
 import fs from "fs"; // Import the file system module
 import path from "path"; // Import the path module
 
-// Function to get the directory of the current module
-function getDirname(moduleUrl: string): string {
-  const { fileURLToPath } = require("url");
-  return fileURLToPath(new URL(".", moduleUrl));
-}
-
-const currentDir = getDirname(import.meta.url); // Get the current directory of this file
-
 // Create the connection with improved connection options
 const poolConnection = mysql.createPool({
   uri: process.env.DATABASE_URL,
@@ -21,8 +13,8 @@ const poolConnection = mysql.createPool({
   queueLimit: 0,
   // Configure SSL to trust the CA certificate
   ssl: {
-    // Provide the path to your CA certificate file
-    ca: fs.readFileSync(path.resolve(currentDir, "../../cert/ca.pem")), // Adjust the path '../certs/ca.crt' based on where you stored the file relative to THIS file
+    // Provide the path to your CA certificate file relative to the project root
+    ca: fs.readFileSync(path.resolve(process.cwd(), "cert/ca.pem")), // <--- MODIFIED LINE
     rejectUnauthorized: true, // Keep this as true to ensure certificate validation
   },
   enableKeepAlive: true,
