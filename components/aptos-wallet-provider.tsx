5e408b0dcb53621e36b2de62212fdb11e9563fc0
pushkar1713 <pushkar1713@gmail.com>
2025-04-25 19:12:35 +0530

    aptos wallet error

--- components/aptos-wallet-provider.tsx
+++ components/aptos-wallet-provider.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { useMemo } from "react";
+import { Network } from "@aptos-labs/ts-sdk";
 
-export function AptosWalletProvider({ children }: { children: React.ReactNode }) {
-  // Use an empty array; wallet-standard will auto-detect Petra, Martian, etc.
-  const wallets = useMemo(() => [], []);
+export function AptosWalletProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <AptosWalletAdapterProvider wallets={wallets} autoConnect={false}>
+    <AptosWalletAdapterProvider
+      autoConnect={false}
+      dappConfig={{ network: Network.TESTNET }}
+    >
       {children}
     </AptosWalletAdapterProvider>
   );
